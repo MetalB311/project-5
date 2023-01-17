346f9b3c062b25bd7853f200802bb2966e26ec29
MetalB <metalb311@gmail.com>
2023-01-16 19:56:10 -0700

    project 5 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header'
+import MainDisplay from './Components/MainDisplay'
+import OptionDisplay from './Components/OptionDisplay'
+import {selectPotentials} from './redux/slices/potentialCountriesSlice'
+import {useSelector} from 'react-redux'
+import {selectDisplay} from './redux/slices/displayCountrySlice'
+
 
 function App() {
+  let potentials = useSelector(selectPotentials)
+  console.log(potentials)
+  let currentDisplay = useSelector(selectDisplay)
+  console.log('DISPLAY', currentDisplay)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App font-link'>
+      <Header />
+      {currentDisplay ? <MainDisplay /> : <OptionDisplay />}
     </div>
-  );
+  )
 }
 
 export default App;
